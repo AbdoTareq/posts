2649c48164a66fa18234a78dc6ac20fc0f8748be
Abdelrahman Tareq <abdotarekfathy@gmail.com>
2023-08-01 17:40:13 +0300

    Update ar.dart

--- lib/utils/langs/ar.dart
+++ lib/utils/langs/ar.dart
@@ -1,150 +1,23 @@
-const String login = 'login';
-const String signup = 'signup';
-const String send = 'send';
-const String sendCode = 'Send Code';
 const String message = 'message';
 const String save = 'save';
-const String speed = 'Speed';
-const String fontSize = 'Font Size';
 const String darkMode = 'Dark Mode';
 const String language = 'Language';
-const String fieldWar = 'Enter some text';
-const String enterCardCodeOrScanIt = 'enter Card Code Or Scan It';
-const String acceptOrder = 'Accept Order';
-const String accept = 'Accept ';
-const String cancelOrder = 'Cancel Order';
 const String cancel = 'Cancel ';
-const String quantity = 'Quantity ';
-const String notifications = 'Notifications';
-const String roomNumber = 'roomNumber ';
-const String updateOrder = 'Update Order';
-const String change_pass = 'change_pass';
-const String orderThisService = 'Order This Service';
-const String request_service = 'request_service';
-const String logout = 'logout';
-const String mail = 'mail';
 const String title = 'Title';
-const String description = 'Description';
 const String yes = 'yes';
-const String resetCode = 'Reset Code';
 const String notes = 'notes';
-const String address = 'address';
-const String pass = 'pass';
-const String orderType = 'orderType';
-const String newPass = 'newPass';
-const String oldPass = 'oldPass';
-const String confirmPass = 'confirmPass';
-const String forgetPass = 'Forget Password';
-const String passWar = 'pass_war';
-const String phoneWar = 'mail_war';
 const String watch = 'watch';
-const String categories = 'categories';
-const String profile = 'profile';
-const String fullName = 'Full Name';
-const String success = 'success';
-const String ser = 'ser';
-const String login_to_continue = 'login_to_continue';
 const String no_data = 'no_data';
-const String phone = 'Phone';
-const String reserve = 'reserve';
-const String product_details = 'product_details';
-const String services_details = 'services_details';
-const String points = 'points';
-const String my_orders = 'my_orders';
-const String cart_order = 'cart_order';
-const String select_job = 'Select Job';
-const String selectCity = 'Select City';
-const String selectArea = 'Select Area';
-const String personal_info = 'personal_info';
-const String ifYouHaveAnAccount = 'if You Have An Account?';
-const String dontHaveAccount = 'Don\'t Have Account?';
-const String enter_fields = 'fields_war';
-const String selectItem = 'selectItem';
-const String user = 'user';
-const String merchant = 'merchant';
-const String client = 'client';
-const String home = 'home';
-const String departments = 'departments';
-const String services = 'services';
-const String news = 'news';
-const String fairs = 'fairs';
-const String le = 'LE';
-const String total = 'total';
-const String contactUs = 'contactUs';
-const String loginAsMerchant = 'loginAsMerchant';
 
 const Map<String, String> ar = {
-  login: 'تسجيل الدخول',
-  signup: 'انشاء حساب',
-  fullName: 'الاسم الكامل',
-  mail: 'البريد الالكتروني',
-  ifYouHaveAnAccount: 'هل لديك حساب؟',
-  phone: 'رقم الهاتف',
-  forgetPass: 'نسيت كلمة المرور؟',
-  logout: 'تسجيل خروج',
-  dontHaveAccount: 'ليس لديك حساب؟',
-  pass: 'كلمة المرور',
-  passWar: 'يجب أن يكون طول النص كلمة السر أكبر 6 حروفٍ/حرفًا',
-  phoneWar: 'رقم الهاتف فارغ',
-  enter_fields: 'ادخل المعلوكات المطلوبة',
-  success: 'تم بنجاح',
-  merchant: 'تاجر',
-  user: 'مستخدم',
-  client: 'عميل',
-  selectItem: 'اختر نوع الحساب',
-  home: 'الرئيسية',
-  departments: 'الاقسام',
-  services: 'الخدمات',
-  news: 'الاخبار',
-  fairs: 'المعارض',
-  contactUs: 'اتصل بنا',
-  loginAsMerchant: 'تسجيل حساب كتاجر',
   save: 'حفظ',
   no_data: 'لا يوجد بيانات',
-  points: 'النقاط',
-  my_orders: 'طلباتي',
-  cart_order: 'طلب الكارت',
-  personal_info: 'بياناتي',
-  reserve: 'حجز',
-  product_details: 'تفاصيل المنتج',
-  confirmPass: 'تأكيد كلمة المرور',
-  oldPass: 'كلمة المرور القديمة',
-  newPass: 'كلمة المرور الجديدة',
-  address: 'العنوان',
-  change_pass: 'تغيير كلمة المرور',
-  services_details: 'تفاصيل الخدمة',
-  request_service: 'طلب خدمة',
-  select_job: 'اختر الوظيفة',
-  selectCity: 'اختر المدينة',
-  selectArea: 'اختر المنطقة',
-  orderType: 'نوع الطلب',
-  send: 'ارسال',
   message: 'رسالة',
   notes: 'ملاحظات',
-  orderThisService: 'طلب هذه الخدمة',
-  sendCode: 'ارسال كود',
-  resetCode: ' كود الارسال',
-  acceptOrder: 'قبول الطلب',
-  cancelOrder: 'الغاء الطلب',
-  updateOrder: 'تحديث الطلب',
   cancel: 'الغاء',
-  accept: 'قبول',
-  enterCardCodeOrScanIt: 'أدخل رمز البطاقة أو قم بالفحص',
-  roomNumber: 'رقم الغرفة',
-  notifications: 'الإشعارات',
-  quantity: 'الكمية',
-  total: 'المجموع',
-  le: 'جنيه',
-  login_to_continue: 'تسجيل الدخول للمتابعة',
   yes: 'نعم',
   watch: 'انتبه',
   title: 'عنوان',
-  description: 'وصف',
-  profile: 'الملف الشخصي',
-  categories: 'الاقسام',
-  fieldWar: "أدخل بعض النص",
-  speed: 'سرعة',
-  fontSize: 'حجم الخط',
   darkMode: 'الوضع المظلم',
   language: 'اللغة',
 };
