e4501a22753e0afb120a27faf994c007c12da6b3
Abdelrahman Tareq <abdotarekfathy@gmail.com>
2023-08-01 15:01:28 +0300

    feat: add pull down to refresh data

--- lib/app/nav/posts/controllers/posts_controller.dart
+++ lib/app/nav/posts/controllers/posts_controller.dart
@@ -6,39 +6,43 @@ import '../../../../../export.dart';
 class PostsController extends GetxController with StateMixin<List<Post>> {
   final PostsRepository repo = PostsRepositoryImp();
   ScrollController scrollController = ScrollController();
-  int pageNum = 0;
+  int _pageNum = 0;
 
   @override
   Future<void> onReady() async {
     await handleRequestWithoutLoading(() async {
       change([], status: RxStatus.loading());
-      await getPosts();
+      await _getPosts();
     }, onError: (e) => change([], status: RxStatus.error(e.message)));
-    getMoreListener();
+    _getMoreListener();
     super.onReady();
   }
 
-  getPosts() async {
-    final res = await repo.getAll(pageNum: pageNum);
+  _getPosts() async {
+    final res = await repo.getAll(pageNum: _pageNum);
     var temp = state!;
     res.fold((_) {}, (r) {
       r.data.forEach((v) {
         temp.add(new Post.fromJson(v));
       });
-
       change(temp, status: RxStatus.success());
     });
   }
 
-  void getMoreListener() {
+  void _getMoreListener() {
     scrollController.addListener(() async {
       if (scrollController.position.maxScrollExtent ==
           scrollController.position.pixels) {
-        pageNum += pageLimit;
+        _pageNum += pageLimit;
         await handleRequest(() async {
-          await getPosts();
+          await _getPosts();
         });
       }
     });
+  }
+
+  refreshFromServer() async {
+    _pageNum = 0;
+    await _getPosts();
   }
 }
--- lib/app/nav/posts/views/posts_view.dart
+++ lib/app/nav/posts/views/posts_view.dart
@@ -8,44 +8,49 @@ class PostsView extends GetView<PostsController> {
   final SearchController searchController = SearchController();
   @override
   Widget build(BuildContext context) {
-    return controller.obx(
-      (state) => Scaffold(
-        backgroundColor:
-            Theme.of(context).textTheme.headlineSmall!.backgroundColor,
-        drawer: AppDrawer(),
-        appBar: CustomAppBar(
-          title: 'Posts',
-          searchList: controller.state,
-          searchController: searchController,
+    return RefreshIndicator(
+      onRefresh: () async => await controller.refreshFromServer(),
+      child: controller.obx(
+        (state) => Scaffold(
+          backgroundColor:
+              Theme.of(context).textTheme.headlineSmall!.backgroundColor,
+          drawer: AppDrawer(),
+          appBar: CustomAppBar(
+            title: 'Posts',
+            searchList: controller.state,
+            searchController: searchController,
+          ),
+          body: ListView.builder(
+            controller: controller.scrollController,
+            itemCount: state!.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Card(
+                child: Row(
+                  children: [
+                    ListTile(
+                      title: state[index].title.text.bold.xl.make(),
+                      subtitle: '${state[index].body.substring(0, 100)} ...'
+                          .text
+                          .bold
+                          .xl
+                          .make(),
+                      onTap: () => pushNewScreen(context,
+                          screen: PostDetailsView(state[index])),
+                    ).expand(),
+                    IconButton(
+                        onPressed: () {}, icon: Icon(Icons.favorite_outline))
+                  ],
+                ),
+              ).p4();
+            },
+          ).hFull(context),
         ),
-        body: ListView.builder(
-          controller: controller.scrollController,
-          itemCount: state!.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Card(
-              child: Row(
-                children: [
-                  ListTile(
-                    title: state[index].title.text.bold.xl.make(),
-                    subtitle: '${state[index].body.substring(0, 100)} ...'
-                        .text
-                        .bold
-                        .xl
-                        .make(),
-                    onTap: () => pushNewScreen(context,
-                        screen: PostDetailsView(state[index])),
-                  ).expand(),
-                  IconButton(
-                      onPressed: () {}, icon: Icon(Icons.favorite_outline))
-                ],
-              ),
-            ).p4();
-          },
-        ).hFull(context),
+        onLoading: ShimmerList(),
+        onError: (error) => ListView(children: [
+          (Get.height * .4).heightBox,
+          error.toString().text.bold.xl.red500.makeCentered().px8()
+        ]),
       ),
-      onLoading: ShimmerList(),
-      onError: (error) =>
-          error.toString().text.bold.xl.red500.makeCentered().px8(),
     );
   }
 }
